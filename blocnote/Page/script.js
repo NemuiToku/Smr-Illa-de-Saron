bcac62febca5e5c32585af4a82e0d9ab01cbdbbd
NemuiToku <josemanuelgfernandez@gmail.com>
2022-06-09 12:06:06 +0200

    Added Card Buttons

--- blocnote/Page/script.js
+++ blocnote/Page/script.js
@@ -32,11 +32,41 @@ $(document).ready(() => {
     .then(data => {
         for (i = 0; i < 100; i++){
             $('#card'+i).append(`
-            Id:${data.rows[i].id}</br>
-            Titulo: ${data.rows[i].title}</br>
-            Texto: ${data.rows[i].text} </br>
+            <div class="row">
+                <div id="cardtext" class="col-9 d-flex align-items-center">
+                    <p style="
+                    margin: 7 0 0 0px;
+                    white-space: nowrap;
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                    max-width: 200px;"
+                    >
+                    Id: ${data.rows[i].id}</br>
+                    Titulo: ${data.rows[i].title}</br>
+                    Texto: ${data.rows[i].text} </br>
+                    </p>
+                </div>
+                <div id="buttons" class="col btn-group-vertical">
+                    <button id="${[i+1]}" onClick="load_click(this.id)" class="btn btn-primary btn-sm" type="button">
+                        <span class="material-symbols-outlined">
+                            upload_file
+                        </span>
+                    </button>
+                    <button id="${[i+1]}" onClick="edit_click(this.id)" class="btn btn-primary btn-sm" type="button">
+                        <span class="material-symbols-outlined">
+                            edit
+                        </span>
+                    </button>
+                    <button id="${[i+1]}" onClick="delete_click(this.id)"class="btn btn-primary btn-sm" type="button">
+                        <span class="material-symbols-outlined">
+                            delete_forever
+                        </span>
+                    </button>
+                </div>
+            </div>
             `)
         }
+        
     })
    })
 
@@ -83,9 +113,6 @@ fetch(url, {
     
 }
 )
-
-
-
 
         });
 
@@ -151,3 +178,64 @@ $("#UpdateSQL").click(() =>{
     $("#alerts").append('<div class="alert alert-warning alert-dismissible fade show m-2"><strong>Success!</strong> - Note updated correctly  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>')
 )
 })
+
+
+function load_click(clicked) {
+    var ID = clicked
+    console.log(ID)
+    var url = "http://localhost:4002/tables/" + ID  
+    fetch(url, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+}}).then((res) => res.json())
+.then((data) => {
+    var titulo = data.row.title
+    var texto = data.row.text
+
+    document.getElementById("title").value = titulo
+    document.getElementById("note").value = texto
+})
+}
+
+function edit_click(clicked) {
+var data = ({
+    "title": document.getElementById("title").value,
+    "text":  document.getElementById("note").value,
+    "id": clicked
+})
+
+var url = "http://localhost:4002/tables/" 
+fetch(url, {
+    method: 'PATCH',
+    mode: 'cors',
+    headers: {
+        'Content-Type': 'application/json'
+      },
+
+body: JSON.stringify(data)
+}).then(
+$("#alerts").append('<div class="alert alert-warning alert-dismissible fade show m-2"><strong>Success!</strong> - Note updated correctly  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>')
+)
+}
+
+function delete_click(clicked) {
+    var ID = clicked
+    console.log(ID)
+    var url = "http://localhost:4002/tables/" + ID  
+    fetch(url, {
+        method: 'DELETE',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+          }
+})
+.then((res) => res.json())
+.then((data) => {
+        console.log(data)
+})
+.then(
+    $("#alerts").append('<div class="alert alert-danger alert-dismissible fade show m-2"><strong>Success!</strong> - Note deleted correctly, please reload the page  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>')
+)
+}
